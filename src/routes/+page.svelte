454ad5672459604dadb373c0c7e270556752eded
ethangeau <yebing@student.unimelb.edu.au>
2023-04-30 00:02:27 +1000

    adjust loyout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="h-screen bg-gray-100">
-  <div class="h-screen flex items-center justify-center">
-    <div class="h-[60vh] w-full max-w-lg p-4 bg-white rounded shadow">
+  <div class="h-screen flex items-left justify-center p-6">
+    <div class="h-[65vh] w-full max-w-lg p-4 bg-white rounded shadow">
       <Chatbox />
       <Prompts />
     </div>
