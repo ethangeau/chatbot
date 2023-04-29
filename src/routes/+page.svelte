fa160b43e66fb29c67fe9b35141960f372c7e63e
ethangeau <yebing@student.unimelb.edu.au>
2023-04-29 21:13:30 +1000

    create skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Chatbox from "../lib/components/Chatbox.svelte";
+  import Prompts from "../lib/components/Prompts.svelte";
+</script>
+
+<div class="h-screen bg-gray-100">
+  <div class="h-screen flex items-center justify-center">
+    <div class="h-[60vh] w-full max-w-lg p-4 bg-white rounded shadow">
+      <Chatbox />
+      <Prompts />
+    </div>
+  </div>
+</div>
